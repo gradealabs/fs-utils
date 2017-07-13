89ccb2303e3235ecec5cc020818acac34ac10e67
Darren Schnare <schnare.d@gmail.com>
2017-07-13 14:42:29 -0400

    feat(rmdirfiles): Add include/exclude options t...
    Add include/exclude options to selectively remove items.

--- src/rmdirfiles.test.ts
+++ src/rmdirfiles.test.ts
@@ -63,4 +63,94 @@ describe('rmdirfiles', function () {
       })
       .catch(done)
   })
+
+  it('should remove exclude files/folders', function (done) {
+    fs.mkdirSync('.rmdirfiles-tmp')
+    fs.appendFileSync('.rmdirfiles-tmp/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/a')
+    fs.mkdirSync('.rmdirfiles-tmp/a/b')
+    fs.appendFileSync('.rmdirfiles-tmp/a/b/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/c')
+
+    rmdirfiles('.rmdirfiles-tmp', { exclude: [ 'a', 'file' ] })
+      .then(() => {
+        assert.ok(fs.existsSync('.rmdirfiles-tmp'))
+        assert.deepStrictEqual(fs.readdirSync('.rmdirfiles-tmp'), [
+          'a', 'file'
+        ])
+      })
+      .then(done, done)
+  })
+
+  it('should only remove items when included', function (done) {
+    fs.mkdirSync('.rmdirfiles-tmp')
+    fs.appendFileSync('.rmdirfiles-tmp/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/a')
+    fs.mkdirSync('.rmdirfiles-tmp/a/b')
+    fs.appendFileSync('.rmdirfiles-tmp/a/b/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/c')
+
+    rmdirfiles('.rmdirfiles-tmp', { include: [ 'a', 'file' ] })
+      .then(() => {
+        assert.ok(fs.existsSync('.rmdirfiles-tmp'))
+        assert.deepStrictEqual(fs.readdirSync('.rmdirfiles-tmp'), [
+          'c'
+        ])
+      })
+      .then(done, done)
+  })
+
+  it('should remove items in the include option, but can still be excluded with exclude option', function (done) {
+    fs.mkdirSync('.rmdirfiles-tmp')
+    fs.appendFileSync('.rmdirfiles-tmp/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/a')
+    fs.mkdirSync('.rmdirfiles-tmp/a/b')
+    fs.appendFileSync('.rmdirfiles-tmp/a/b/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/c')
+
+    rmdirfiles('.rmdirfiles-tmp', { include: [ 'a', 'file' ], exclude: [ 'a' ] })
+      .then(() => {
+        assert.ok(fs.existsSync('.rmdirfiles-tmp'))
+        assert.deepStrictEqual(fs.readdirSync('.rmdirfiles-tmp'), [
+          'a', 'c'
+        ])
+      })
+      .then(done, done)
+  })
+
+  it('should remove no items when include option is an empty array', function (done) {
+    fs.mkdirSync('.rmdirfiles-tmp')
+    fs.appendFileSync('.rmdirfiles-tmp/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/a')
+    fs.mkdirSync('.rmdirfiles-tmp/a/b')
+    fs.appendFileSync('.rmdirfiles-tmp/a/b/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/c')
+
+    rmdirfiles('.rmdirfiles-tmp', { include: [] })
+      .then(() => {
+        assert.ok(fs.existsSync('.rmdirfiles-tmp'))
+        assert.deepStrictEqual(fs.readdirSync('.rmdirfiles-tmp'), [
+          'a', 'c', 'file'
+        ])
+      })
+      .then(done, done)
+  })
+
+  it('should ignore invalid include/exclude values', function (done) {
+    fs.mkdirSync('.rmdirfiles-tmp')
+    fs.appendFileSync('.rmdirfiles-tmp/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/a')
+    fs.mkdirSync('.rmdirfiles-tmp/a/b')
+    fs.appendFileSync('.rmdirfiles-tmp/a/b/file', '', { encoding: 'utf8' })
+    fs.mkdirSync('.rmdirfiles-tmp/c')
+
+    rmdirfiles('.rmdirfiles-tmp', <any>{ include: [ 'a', /file/, 45 ], exclude: [ 'a', 12 ] })
+      .then(() => {
+        assert.ok(fs.existsSync('.rmdirfiles-tmp'))
+        assert.deepStrictEqual(fs.readdirSync('.rmdirfiles-tmp'), [
+          'a', 'c'
+        ])
+      })
+      .then(done, done)
+  })
 })
--- src/rmdirfiles.ts
+++ src/rmdirfiles.ts
@@ -2,15 +2,64 @@ import * as fs from 'fs'
 import * as path from 'path'
 import rmdir from './rmdir'
 
+interface RmDirFilesOptions {
+  include?: Array<string | RegExp>
+  exclude?: Array<string | RegExp>
+}
+
 /**
  * Attempts to delete all the files from a directory, but not the directory
  * itself.
  *
+ * Optionally, files directly in the dir can be explicitly included/excluded
+ * from being removed. Use either the include or exclude options to determine
+ * the file names to include/exclude from the removal operation. These options
+ * accept an array of strings or RegExp instances.
+ *
+ * NOTE: The exclude option takes precedence over include.
+ *
+ * Example:
+ * // Remove all files except the .git folder
+ * rmdirfiles('dist', { exclude: [ '.git' ] })
+ *
+ * * // Remove all files except the .git folder and any file starting with '_'
+ * rmdirfiles('dist', { exclude: [ '.git', /^_/ ] })
+ *
  * @param dir The directory to delete all files from
+ * @param options The options to modify the operation
  * @return A promise that resolves to void.
  */
-export default function rmdirfiles (dir: string): Promise<void> {
+export default function rmdirfiles (dir: string, options: RmDirFilesOptions = {}): Promise<void> {
+  const { include = [ /./ ] , exclude = [] } = options
+
   return readdir(dir)
+    .then(files => files.filter(f => {
+      return include.some(x => {
+        if (typeof x === 'string') {
+          return f === x
+        } else if (x instanceof RegExp) {
+          return x.test(f)
+        } else {
+          return false
+        }
+      })
+    }))
+    .then(files => {
+      return exclude.length
+        ? files.filter(f => {
+          return !exclude.some(x => {
+            if (typeof x === 'string') {
+              return f === x
+            } else if (x instanceof RegExp) {
+              return x.test(f)
+            } else {
+              return false
+            }
+          })
+        })
+        : files
+    })
+    .then(files => files.map(f => path.join(dir, f)))
     .then(files => {
       return Promise.all(files.map(stat))
         .then(statsList => {
@@ -58,7 +107,7 @@ function readdir (dir: string): Promise<string[]> {
       if (error) {
         reject(error)
       } else {
-        resolve(files.map(f => path.join(dir, f)))
+        resolve(files)
       }
     })
   })
